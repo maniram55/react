e58e9b6e1b81061ee7740eeaa5dee97c17305206
maniram55 <manikrish5793@gmail.com>
2021-02-12 20:46:51 +0530

    Tagify task work in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import TagList from './TagList';
+import React, { Component } from "react";
 
-function App() {
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      isTagFocused: false,
+      selectedItems: []
+      };
+    }
+
+    onParantSelectMember = (slectedMember) => {
+      this.setState({
+        selectedItems: this.state.selectedItems.concat(slectedMember)
+      })
+    this.toggleFocus();
+  }
+
+    toggleFocus = () => {
+    const isTagFocused = !this.state.isTagFocused 
+    this.setState({ isTagFocused });
+    }
+
+    selectedMembers = () => {
+      const listItems = this.state.selectedItems.map((currentItem) => {
+         return <div>
+         <div className="">
+         <div className="" >
+             <img src={currentItem.avatar} alt= {currentItem.name}  width="15" height="15"></img>
+            <span className=""> 
+            {currentItem.name}
+           </span>
+            </div>
+            </div>
+            </div>
+        })
+      return listItems;
+    }
+
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>My Tagify Elements</h1>
       </header>
+
+      <div className="input-container">
+      <div>{this.selectedMembers()}</div>
+      <input name='input' className='some_class_name' placeholder='Add Some Tags....'
+      onFocus={this.toggleFocus}/> 
+      </div>
+      {this.state.isTagFocused ? <TagList onChildSelectMember= {this.onParantSelectMember}/> : null}
     </div>
   );
 }
+}
 
-export default App;
